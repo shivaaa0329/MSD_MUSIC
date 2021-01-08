5d0619c5e43083e5e1d55523a64e7c885eadfacc
shivaaa0329 <76025599+shivaaa0329@users.noreply.github.com>
2021-01-08 14:08:21 +0530

    Update PlayScreen.js

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -63,7 +63,7 @@ export default class PlayScreen extends React.Component {
                     <div className="playhead">
                         <h1 className="playheadplay">Play</h1>
                         <Link to="../">
-                            <img alt="MSD Music" src="https://telegra.ph/file/6b6f1926afdbe148291cf.png" className="playlogobtn" width="40" height="40" />
+                            <img alt="MSD Music" src="https://telegra.ph/file/6b6f1926afdbe148291cf.png" className="playlogobtn" width="50" height="50" />
                         </Link>
                     </div>
                     <div className="playcontent"><br /><br />
